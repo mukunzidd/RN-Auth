24c6c18dc41812ebf81c89beb0f3b1f09fa4c9b6
dodo <dmukunzi0@gmail.com>
2017-11-09 13:56:35 +0200

    ReUse reausable/common components

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header, Card } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header headerText="RN-Auth" />
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -15,9 +15,5 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/common/CardSection.js
@@ -0,0 +1,28 @@
+// import for making a component
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// make a component
+const CardSection = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    )
+};
+
+// Styles
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        flexDirection: 'row',
+        position: 'relative',
+        elevation: 1,
+    }
+};
+
+// make the component available to the other partsss of the app
+export { CardSection };
